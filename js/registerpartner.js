c5223d7093c7c5e116a1e69d42872e4eeefaa660
Eric <elymberopoulos@luc.edu>
2018-12-10 15:12:49 -0600

    cleaning up the code a bit. added some comments

--- js/registerpartner.js
+++ js/registerpartner.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
 
-    //extract whatever is inside of the script tag with an id of employee-modal-template
-
+    //HARD CODED ENTRY POINT FOR PARTNERS
     var partnerResourceURI = "http://localhost:8081/partner"
-    //http://localhost:8081/partner
 
 
-    //submit the add employee form to the server
     $('form').submit(function (event) {
 
-        // /**
-        //  * salary and privilege are not sent to the server since there is no endpoint that accepts those parameters
-        //  * @type {{firstName: *, lastName: *}}
-        //  */
+
         var formData = {
             'userID': getUserID(),
             'firstName': getfirstName(),
